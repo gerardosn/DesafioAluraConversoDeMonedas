ebc4f32aba9bfa008d2702cd72e12237ba1e7807
gerardosn <gerardo.sena@gmail.com>
2024-10-07 18:46:17 -0300

    se implementa Dotenv en java

--- src/main/java/sn/gerardo/ConversionRates.java
+++ src/main/java/sn/gerardo/ConversionRates.java
@@ -1,4 +1,44 @@
 package sn.gerardo;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class ConversionRates {
+     private final Dotenv dotenv = Dotenv.configure().load();
+     private final String API_KEY = dotenv.get("API_KEY");
+    private  final String API_URL = "https://v6.exchangerate-api.com/v6/"+API_KEY+"/latest/";
+
+    public double consultarTasaCambio(String origenMoneda, String destinoMoneda) {
+        try {
+            String urlString = API_URL + origenMoneda;
+            URL url = new URL(urlString);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String inputLine;
+            StringBuffer content = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
+            }
+
+            in.close();
+            conn.disconnect();
+
+            Gson gson = new Gson();
+            JsonObject json = gson.fromJson(content.toString(), JsonObject.class);
+            return json.getAsJsonObject("conversion_rates").get(destinoMoneda).getAsDouble();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
 }
--- src/main/java/sn/gerardo/Main.java
+++ src/main/java/sn/gerardo/Main.java
@@ -1,7 +1,28 @@
 package sn.gerardo;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese la moneda de origen: ");
+        String origenMoneda = scanner.nextLine().toUpperCase();
+
+        System.out.println("Ingrese el monto: ");
+        double monto = scanner.nextDouble();
+
+        scanner.nextLine(); // Clear the buffer
+
+        System.out.println("Ingrese la moneda destino: ");
+        String destinoMoneda = scanner.nextLine().toUpperCase();
+
+        ConversionRates conversionRates = new ConversionRates();
+        double tasaCambio = conversionRates.consultarTasaCambio(origenMoneda, destinoMoneda);
+
+        double montoConvertido = monto * tasaCambio;
+        System.out.println("El monto convertido es: " + montoConvertido + " " + destinoMoneda);
+
     }
 }
